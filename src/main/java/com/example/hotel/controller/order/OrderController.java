6fdf4dbb950e6c176faa1ac1a79365cc17e48868
Guo <181250040@smail.nju.edu.cn>
2020-06-17 11:24:12 +0800

    feat: account修改

--- src/main/java/com/example/hotel/controller/order/OrderController.java
+++ src/main/java/com/example/hotel/controller/order/OrderController.java
@@ -1,6 +1,5 @@
 package com.example.hotel.controller.order;
 
-import com.example.hotel.bl.hotel.HotelService;
 import com.example.hotel.bl.order.OrderService;
 import com.example.hotel.vo.OrderVO;
 import com.example.hotel.vo.ResponseVO;
@@ -19,7 +18,6 @@ import java.text.ParseException;
 @RestController()
 @RequestMapping("/api/order")
 public class OrderController {
-    private HotelService hotelService;
     @Autowired
     private OrderService orderService;
 
