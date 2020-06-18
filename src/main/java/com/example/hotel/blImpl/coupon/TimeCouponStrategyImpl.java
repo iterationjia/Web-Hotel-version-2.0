a7cda4dd33c26b5aa70ae27c4f7bffa3c52ab0c4
闫玮琦 <181250170@smail.nju.edu.cn>
2020-06-18 18:47:51 +0800

    feat:0.0

--- src/main/java/com/example/hotel/blImpl/coupon/TimeCouponStrategyImpl.java
+++ src/main/java/com/example/hotel/blImpl/coupon/TimeCouponStrategyImpl.java
@@ -18,11 +18,11 @@ public class TimeCouponStrategyImpl implements CouponMatchStrategy {
     @Override
     public boolean isMatch(OrderVO orderVO, Coupon coupon) {
         //DateTimeFormatter formatter = DateTimeFormatter.ofPattern("YYYY-MM-dd");
-        String time1 = "2020-11-11 00:00:00";
-        DateTimeFormatter dtf1 = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String time1 = "2020-11-11";
+        DateTimeFormatter dtf1 = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         LocalDateTime start = LocalDateTime.parse(time1, dtf1);
-        String time2 = "2020-11-14 00:00:00";
-        DateTimeFormatter dtf2 = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String time2 = "2020-11-17";
+        DateTimeFormatter dtf2 = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         LocalDateTime end = LocalDateTime.parse(time2, dtf2);
         if (coupon.getCouponType() == 4 && coupon.getStartTime().isAfter(start) && coupon.getEndTime().isBefore(end)){
             return true;
