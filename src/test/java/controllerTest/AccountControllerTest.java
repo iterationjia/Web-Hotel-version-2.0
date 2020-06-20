0674987a50faa97b36169e89a44af85d8a2b8c18
Guo <181250040@smail.nju.edu.cn>
2020-06-20 22:36:14 +0800

    fix: admintest bug

--- src/test/java/controllerTest/AccountControllerTest.java
+++ src/test/java/controllerTest/AccountControllerTest.java
@@ -1,3 +1,4 @@
+package controllerTest;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
--- src/test/java/controllerTest/AdminControllerTest.java
+++ src/test/java/controllerTest/AdminControllerTest.java
@@ -1,3 +1,5 @@
+package controllerTest;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -44,20 +46,13 @@ public class AdminControllerTest {
                 "        \"phoneNumber\": \"123\",\n" +
                 "        \"credit\": 10000\n" +
                 "        }";
-        // 前端向后端传的对象以JSON格式传送
-        // 嫌写转义符麻烦的话，可以新建一个json文件写好，然后用idea复制，idea会自动帮你加转义符
+
         String res = mockMvc.perform(
                 post("/api/admin/addManager").contentType(MediaType.APPLICATION_JSON).content(managerJson)
-                // 这里是模拟向后端发起一个post请求 url写上，JSON传对象，内容就是orderJson
-                // 如果是get请求就写get，后边content啥的应该不用写
-                // 请求的参数如果在url里，那就直接在url写上对应的数据
-                /*
-                如果是get请求**传参**(不是url传参)，则写get("url").contentType(MediaType.APPLICATION_FORM_URLENCODED).param("key","value")
-                有几个param就在后面点几个param，不过这种方式似乎只有搜索酒店用到了
-                 */
+
         ).andReturn().getResponse().getContentAsString();
-        // 这里是接收请求的返回值，也就是ResponseVO，以字符串形式
+
         assertTrue(new JSONObject(res).getBoolean("success"));
-        // ResponseVO里面有Boolean success, String message, Object content三个成员，我判断success是不是true就知道测试是否成功
+
     }
 }
